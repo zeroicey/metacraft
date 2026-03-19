198a358727806eed6e9785c53df015c37c4bfdeb
zeroicey <zeroicey.hp@outlook.com>
2026-03-20 01:01:14 +0800

    feat: upload the rest of the files

--- apps/web/src/providers/QueryProvider.tsx
+++ apps/web/src/providers/QueryProvider.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from "react";
 
 export default function QueryProvider({
@@ -7,7 +8,10 @@ export default function QueryProvider({
   children: React.ReactNode;
 }) {
   const [queryClient] = React.useState(() => new QueryClient());
+  // <ReactQueryDevtools initialIsOpen={false} />
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
   );
 }
